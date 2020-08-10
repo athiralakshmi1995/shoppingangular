d40af65a2ff89503191aec9a822891950ea7d9a7
ATHIRA LAKSHMI <68342911+athiralakshmi1995@users.noreply.github.com>
2020-08-10 11:51:00 +0530

    Add files via upload

---
+++ shoppingcart.component.ts
@@ -0,0 +1,37 @@
+import { Component, OnInit } from '@angular/core';
+import { ShoppingService} from '../shopping.service'
+// import { createTokenForExternalReference } from '@angular/compiler/src/identifiers';
+
+@Component({
+  selector: 'app-shoppingcart',
+  templateUrl: './shoppingcart.component.html',
+  styleUrls: ['./shoppingcart.component.css']
+})
+export class ShoppingcartComponent implements OnInit {
+
+  constructor( private obj:ShoppingService) { }
+  list:any=[];
+  
+x:any
+sum: string
+  ngOnInit(): void {
+  
+  this.obj.getData().subscribe((x)=>{
+    console.log("json array",x)
+   this.list=x
+  console.log("arry",this.list)
+
+  })
+//   console.log("array js",this.list)
+  }
+addtocart(product){
+ this.obj.cart.push(product)
+ console.log(this.obj.cart)
+ }
+
+   
+
+ }
+
+
+
